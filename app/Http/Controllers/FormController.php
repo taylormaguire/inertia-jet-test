0eec4de7734c9b52cd0d3a3d1114ab01d63674e9
Taylor Maguire <taylor@codingfoundry.com>
2021-04-19 10:58:16 -0700

    Initial commit

---
+++ app/Http/Controllers/FormController.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Form;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redirect;
+use Inertia\Inertia;
+
+class FormController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Inertia\Response
+     */
+    public function create()
+    {
+        return Inertia::render('Form/Create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        Form::create(
+            $request->validate([
+                'name' => ['required', 'max:50'],
+                'email' => ['required', 'max:50', 'email'],
+            ])
+        );
+
+        return 'done';
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Form  $form
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Form $form)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Form  $form
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Form $form)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Form  $form
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Form $form)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Form  $form
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Form $form)
+    {
+        //
+    }
+}
---
+++ resources/js/Pages/Form/Create.vue
@@ -0,0 +1,51 @@
+<template>
+    <app-layout>
+        <template #header>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                Create Form
+            </h2>
+        </template>
+
+        <div>
+            <div class="max-w-7xl mx-auto py-10 sm:px-6 lg:px-8">
+                <form @submit.prevent="submit">
+                    <label for="first_name">Name:</label>
+                    <input id="first_name" v-model="form.name" />
+                    <div v-if="errors.name">{{ errors.name }}</div>
+
+                    <label for="email">Email:</label>
+                    <input id="email" v-model="form.email" />
+                    <div v-if="errors.email">{{ errors.email }}</div>
+
+                    <button type="submit">Submit</button>
+                </form>
+            </div>
+        </div>
+    </app-layout>
+</template>
+
+<script>
+import AppLayout from '@/Layouts/AppLayout'
+
+export default {
+    components: {
+        AppLayout,
+    },
+    props: {
+        errors: Object,
+    },
+    data() {
+        return {
+            form: {
+                name: null,
+                email: null,
+            },
+        }
+    },
+    methods: {
+        submit() {
+            this.$inertia.post('/form', this.form)
+        },
+    },
+}
+</script>
